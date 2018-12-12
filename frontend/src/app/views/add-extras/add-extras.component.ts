fd6d8d9a30bcfbe6bd732950be51bbb7951652c8
Josiah Goodson <jgoodson@hortonworks.com>
2018-12-12 15:06:51 -0600

    Latest add extras

--- frontend/src/app/views/add-extras/add-extras.component.ts
+++ frontend/src/app/views/add-extras/add-extras.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit, NgModule } from '@angular/core';
 import { Router } from '@angular/router'
 
+interface Recipe {
+  recipeType: string,
+  clusterType: string,
+  clusterVersion: string,
+  service: string,
+  code: string
+}
+
 @Component({
   templateUrl: 'add-extras.component.html'
 })
@@ -10,14 +18,22 @@ export class AddExtrasComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  types: String[] = ['Recipe','NiFi Template', 'Zeppelin Notebook', 'SQL']
+  extraTypes: String[] = ['Recipe','NiFi Template', 'Zeppelin Notebook', 'SQL']
+  recipeTypes: String[] = ['Pre-Ambari','Post-Ambari', 'Post-cluster install', 'On termination']
+  clusterTypes: String[] = ['HDP', 'HDF', 'HDP+HDF']
+  serviceTypes: String[] = []
+
 
   public showRecipe = true;
   public showNifi = true;
   public showZep = true;
   public showSQL = true;
 
-  addExtra(form) {
+  addExtra({value, valid}:{value: Recipe, valid: boolean}) {
+    console.log(value)
+  }
+
+  selectExtra() {
 
   }
 }
