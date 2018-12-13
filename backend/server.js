71376413ab57ce2fb5c42302884255cba8e640f2
Josiah Goodson <jgoodson@hortonworks.com>
2018-12-13 11:30:26 -0600

    Add pushing blueprint to CB

--- backend/server.js
+++ backend/server.js
@@ -71,43 +71,17 @@ router.route('/cluster/:id').get((req, res) => {
         })
 });
 
-
-router.route('/get_token/:username/:password/:cb_url', function (req, res) {
-
-    // process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0; 
-
-    // var request = require("request");
-
-    // var options = { method: 'POST',
-    //   url: 'https://192.168.56.100/identity/oauth/authorize',
-    //   qs: 
-    //    { 'response_type': 'token',
-    //      'client_id': 'cloudbreak_shell',
-    //      'scope.0': 'openid',
-    //      'source': 'login',
-    //      'redirect_uri': 'http://cloudbreak.shell' },
-    //   headers: 
-    //    { 'accept': 'application/x-www-form-urlencoded' },
-    //   data: 'credentials={"username":"pvidal@hortonworks.com","password":"HWseftw33#"}' };
-
-    // request(options, function (error, response, body) {
-    //   if (error) throw new Error(error);
-    //   //res.send(body);
-    //   var token = JSON.stringify(response);
-    //   res.send(token);
-    // });
-
+router.route('/get_token').post((req, res) => {
     var exec = require('child_process').exec;
+    console.log(req.body);
 
-    function puts(error, stdout, stderr) { sys.puts(stdout) }
-    exec("curl -k -iX POST -H \"accept: application/x-www-form-urlencoded\" -d 'credentials={\"username\":\"" + req.params.username + "\",\"password\":\"" + req.params.password + "\"}' \"https://" + req.params.cb_url + "/identity/oauth/authorize?response_type=token&client_id=cloudbreak_shell&scope.0=openid&source=login&redirect_uri=http://cloudbreak.shell\" | grep location | cut -d'=' -f 3 | cut -d'&' -f 1", function (error, stdout, stderr) {
+    exec("curl -k -iX POST -H \"accept: application/x-www-form-urlencoded\" -d 'credentials={\"username\":\"" + req.body.username + "\",\"password\":\"" + req.body.password + "\"}' \"https://" + req.body.cb_url + "/identity/oauth/authorize?response_type=token&client_id=cloudbreak_shell&scope.0=openid&source=login&redirect_uri=http://cloudbreak.shell\" | grep location | cut -d'=' -f 3 | cut -d'&' -f 1", function (error, stdout, stderr) {
         if (!error) {
-            res.send(stdout);
+            res.json(stdout);
         } else {
-            res.send('ERROR: ' + stderr);
+            res.json('ERROR: ' + stderr);
         }
     });
-
 })
 
 router.get('/load_recipe/:cb_url/:recipe_name/:recipe_type/:recipe_base64/:token', function (req, res) {
@@ -146,18 +120,18 @@ router.get('/load_recipe/:cb_url/:recipe_name/:recipe_type/:recipe_base64/:token
 
 router.route('/load_blueprint').post((req, res) => {
 
-
     // Expects the following in body: cb_url, token, bp_base64, cluster_name
     process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
     var request = require("request");
-    
+    console.log("Token",req.body.token)
     var options = {
         method: 'POST',
         url: ''+ req.body.cb_url + '/cb/api/v1/blueprints/user',
+        
         headers:
         {
             'cache-control': 'no-cache',
-            Authorization: 'Bearer ' + req.body.token,
+            'Authorization': 'Bearer ' + req.body.token,
             'Content-Type': 'application/json'
         },
         body:
@@ -221,10 +195,7 @@ router.route('/services/:cluster_type/:description').get((req, res) => {
             console.log('ERROR:', error)
         })
 });
-router.route('/set_custom_recipe').post((req, res) => {
-    
-
-    
+router.route('/set_custom_recipe').post((req, res) => {   
     db.any('insert into cloudbreak_cuisine.components_recipes (service_id, recipe_description, extra_type, '+req.body.recType+') values (\''+ req.body.service_id + '\',\'' + req.body.recipe_description + '\',\'' + req.body.extra_type + '\',\'' + req.body.recipe + '\')')
         .then(data => {
             res.json(data);
